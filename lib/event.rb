174ac941ccaeae15dbf32bb67d3c1c546b61bb5e
Lilas <55747074+LilasAll@users.noreply.github.com>
2019-10-14 16:46:24 +0200

    Update event.rb

--- lib/event.rb
+++ lib/event.rb
@@ -1,27 +1,6 @@
 require 'pry' 
 require 'time'
 
-class User
-
-	#@@user_name = self.class
-	attr_accessor :email, :age
-	@@all_users = Array.new
-	
-	def initialize(user_email, user_age)
-		@email = user_email
-		@age = user_age
-		@@all_users << self
-	end
-
-	def self.all
-		return @@all_users
-	end
-
-	def find_by_email(email)
-		return User
-	end
-
-end
 
 class Event
 	#create a list of attendees
